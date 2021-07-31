f446380e52bb38db99202ea3a106a9d79a5d62bc
motohiro takagi <tkg.8.mthr@gmail.com>
2021-07-31 09:42:06 +0900

    表示の並び順の変更とinlineのcssを適用

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,7 +8,7 @@ class BlogsController < ApplicationController
 
   # GET /blogs/1 or /blogs/1.json
   def show
-    @schedules = @blog.schedules
+    @schedules = @blog.schedules.order(created_at: :desc)
     @schedule = @blog.schedules.build
   end
 
